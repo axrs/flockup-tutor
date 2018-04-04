63cbf81135bbfbaeb98c075cb15775f2ba3b7017
Alexander Scott <xander@cadcoder.com>
2018-04-04 20:56:38 +1000

    Warn about missing api key

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,46 @@
-import 'package:flutter/material.dart';
+import 'package:feather/feather.dart';
 import 'package:flockup/config.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  final AppDbStream stateStream = new AppDbStream(AppDb.onUpdate);
+
   @override
   Widget build(BuildContext context) {
+    if (MEETUP_API_KEY == "") {
+      debugPrint("=======================================================");
+      debugPrint("MEETUP_API_KEY is missing. Please set it in config.dart");
+      debugPrint("=======================================================");
+    }
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(MEETUP_API_KEY),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              MEETUP_API_KEY,
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: 'Flockup',
+        theme: new ThemeData(
+          primarySwatch: Colors.red,
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
+        home: new StreamBuilder<Map>(
+          stream: stateStream,
+          initialData: AppDb.init({"loading": true}).store,
+          builder: (context, snapshot) => buildHome(context, snapshot.data),
+        ));
   }
+}
+
+Widget buildHome(BuildContext context, Map data) {
+  return new Scaffold(
+    appBar: new AppBar(
+      title: new Text("Flockup"),
+    ),
+    body: new Center(
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Expanded(
+            child: new Text("Events will go here"),
+          )
+        ],
+      ),
+    ),
+  );
 }
